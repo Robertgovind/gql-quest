9453c57a53b62ec9e071ef1375a0229bfa0db4b0
Goivnd Kumar Yadav <robertgovind@gmail.com>
2025-07-22 00:57:20 +0545

    graphql codes: making server and schemas

---
+++ 1.GraphQL-the-journey-begins/index.js
@@ -0,0 +1,38 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import typeDefs from "./schema.js";
+import { games, authors, reviews } from "./db.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return games;
+    },
+    reviews() {
+      return reviews;
+    },
+    authors() {
+      return authors;
+    },
+    review(parent, args) {
+      return reviews.find((review) => review.id === args.id);
+    },
+    game(parent, args) {
+      return games.find((game) => game.id === args.id);
+    },
+    author(parent, args) {
+      return authors.find((author) => author.id === args.id);
+    },
+  },
+};
+// server setup
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server is running at ${url}`);
